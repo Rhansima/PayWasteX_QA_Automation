a6cd4c4d61b56ab00cf43d24b86e83438f396484
Tharushi Hansima <tharushihansima23@gmail.com>
2025-08-04 16:25:21 +0530

    add allure reports

--- src/test/java/tests/allure/RegisterTest.java
+++ src/test/java/tests/allure/RegisterTest.java
@@ -1,15 +1,22 @@
-package tests;
+package tests.allure;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class RegisterTest {
+
     WebDriver driver;
 
     @BeforeClass
@@ -20,21 +27,25 @@ public class RegisterTest {
         driver.manage().window().maximize();
     }
 
-    @Test
+    @Test(description = "Click 'Create An Account' and verify redirection")
+
+    @Severity(value = SeverityLevel.CRITICAL)
+
+    @Story("Navigate from Login to Registration")
+
+    @Description("Test checks if 'Create An Account' button correctly navigates user to register page")
+
     public void testClickCreateAccount() throws InterruptedException {
         WebElement createAcc = driver.findElement(By.xpath("//a[contains(text(),'Create An Account')]"));
         createAcc.click();
 
-
-        String expectedUrl = "http://localhost:5173/login";
+        String expectedUrl = "http://localhost:5173/login"; // Correct target URL?
         String currentUrl = driver.getCurrentUrl();
 
         Assert.assertEquals(currentUrl, expectedUrl, "Navigation to Register page failed!");
 
-        Thread.sleep (3000);
+        Thread.sleep(3000);
     }
-
-
 
     @AfterClass
     public void tearDown() {
